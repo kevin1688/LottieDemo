ff3cc709122846b7836fee0898d08bdbbc9ba750
kb18k <jackexi@gmail.com>
2021-10-05 18:43:23 +0800

    Initial Commit

---
+++ LottieDemo/LottieDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieDemoApp.swift
+//  LottieDemo
+//
+//  Created by kai wen chen on 2021/10/5.
+//
+
+import SwiftUI
+
+@main
+struct LottieDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
